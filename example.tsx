962b7d16b52230aeae4e41dd0361c16571d6c7f6
zhenghaohe <iron4wine@outlook.com>
2020-02-03 22:19:19 -0800

    added the main page(WIP)

---
+++ example.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+   <header>
+     <div className='logo'>
+       Chakras
+     </div>
+   </header>
+    <div>
+      <aside>
+        <h2>Components</h2>
+        <ul>
+          <li>
+          </li>
+          <li>
+          </li>
+        </ul>
+      </aside>
+      <main>
+
+      </main>
+    </div>
+  </div>,
+  document.querySelector('#root')
+);
+
